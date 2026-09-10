7e4acafd0025f54b06cb581816c3140182eb7fdd
Arpit Aryan <148412988+ArpitArya29@users.noreply.github.com>
2026-09-10 17:57:15 +0530

    [3271] [Time Beats: 97.32%] [Memory Beats: 44.52%] - Leet2Hub

--- 04-String-Manipulation/3271. Hash Divided String/3271. Hash Divided String.java
+++ 04-String-Manipulation/3271. Hash Divided String/3271. Hash Divided String.java
@@ -1,21 +1,23 @@
 1class Solution {
 2    public String stringHash(String s, int k) {
-3        StringBuilder sb = new StringBuilder();
-4
-5        int len = s.length();
+3        //Adding comment to check github workflow
+4         
+5        StringBuilder sb = new StringBuilder();
 6
-7        for(int i=0; i<len; i+=k) {
-8            int hashVal = 0;
-9            
-10            for(int j=i; j<i+k; j++) {
-11                hashVal += s.charAt(j) - 'a';
-12            }
-13
-14            hashVal %= 26;
+7        int len = s.length();
+8
+9        for(int i=0; i<len; i+=k) {
+10            int hashVal = 0;
+11            
+12            for(int j=i; j<i+k; j++) {
+13                hashVal += s.charAt(j) - 'a';
+14            }
 15
-16            sb.append((char)(hashVal + 'a'));
-17        }
-18
-19        return sb.toString();
-20    }
-21}
+16            hashVal %= 26;
+17
+18            sb.append((char)(hashVal + 'a'));
+19        }
+20
+21        return sb.toString();
+22    }
+23}
