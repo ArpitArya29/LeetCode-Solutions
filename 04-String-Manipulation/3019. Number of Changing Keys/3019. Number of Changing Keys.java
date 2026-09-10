37477fc2026ffa801b19aa14004bb989029135df
Arpit Aryan <148412988+ArpitArya29@users.noreply.github.com>
2026-09-10 17:48:41 +0530

    [3019] [Time Beats: 100.00%] [Memory Beats: 28.02%] - Leet2Hub

--- 04-String-Manipulation/3019. Number of Changing Keys/3019. Number of Changing Keys.java
+++ 04-String-Manipulation/3019. Number of Changing Keys/3019. Number of Changing Keys.java
@@ -1,21 +1,22 @@
 1class Solution {
 2    public int countKeyChanges(String s) {
-3        int len = s.length();
-4
-5        String lcStr = s.toLowerCase();
-6
-7        char prev = lcStr.charAt(0);
-8
-9        int cnt = 0;
-10
-11        for(int i=1; i<len; i++) {
-12            char curr = lcStr.charAt(i);
-13
-14            if(prev != curr) cnt++;
-15
-16            prev = curr;
-17        }
-18
-19        return cnt;
-20    }
-21}
+3        // Adding some comments to verify github workflow
+4        int len = s.length();
+5
+6        String lcStr = s.toLowerCase();
+7
+8        char prev = lcStr.charAt(0);
+9
+10        int cnt = 0;
+11
+12        for(int i=1; i<len; i++) {
+13            char curr = lcStr.charAt(i);
+14
+15            if(prev != curr) cnt++;
+16
+17            prev = curr;
+18        }
+19
+20        return cnt;
+21    }
+22}
